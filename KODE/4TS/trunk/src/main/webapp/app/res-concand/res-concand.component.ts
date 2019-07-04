5127e3ccce88cec9c1838ffdf76a9a71a9e9069c
svnuser2 <svnuser2@07dfc717-5244-0410-b0d6-a68f8053c131>
2019-07-04 19:24:39 +0000

    ajustar orden de tablas en Detalle del candidato

--- KODE/4TS/trunk/src/main/webapp/app/res-concand/res-concand.component.ts
+++ KODE/4TS/trunk/src/main/webapp/app/res-concand/res-concand.component.ts
@@ -1,8 +1,8 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 // Inicio datatable
-import {MatSort} from '@angular/material/sort';
-import {MatPaginator} from '@angular/material/paginator';
-import {MatTableDataSource} from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 export interface PeriodicElement {
   Id: number;
@@ -25,33 +25,6 @@ export interface Skills {
   Eliminar: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {Id: 1, Tarea: 'Abierto', Creador: 'Capgemini', Destinatario: 'Java', FechaAlta: 'Junior', Estatus: 'MABE'},
-  {Id: 2, Tarea: 'Abierto', Creador: 'Capgemini', Destinatario: 'Java', FechaAlta: 'Junior', Estatus: 'MABE'},
-  {Id: 3, Tarea: 'Abierto', Creador: 'Capgemini', Destinatario: 'Java', FechaAlta: 'Junior', Estatus: 'MABE'},
-  {Id: 4, Tarea: 'Abierto', Creador: 'Capgemini', Destinatario: 'Java', FechaAlta: 'Junior', Estatus: 'MABE'},
-  {Id: 5, Tarea: 'Abierto', Creador: 'Capgemini', Destinatario: 'Java', FechaAlta: 'Junior', Estatus: 'MABE'},
-  {Id: 3, Tarea: 'Abierto', Creador: 'Capgemini', Destinatario: 'Java', FechaAlta: 'Junior', Estatus: 'MABE'},
-  {Id: 6, Tarea: 'Abierto', Creador: 'Capgemini', Destinatario: 'Java', FechaAlta: 'Junior', Estatus: 'MABE'},
-  {Id: 7, Tarea: 'Abierto', Creador: 'Capgemini', Destinatario: 'Java', FechaAlta: 'Junior', Estatus: 'MABE'},
-  {Id: 8, Tarea: 'Abierto', Creador: 'Capgemini', Destinatario: 'Java', FechaAlta: 'Junior', Estatus: 'MABE'},
-  {Id: 9, Tarea: 'Cerrado', Creador: 'Capgemini', Destinatario: 'Java', FechaAlta: 'Junior', Estatus: 'MABE'},
-  {Id: 10, Tarea: 'Cerrado', Creador: 'Capgemini', Destinatario: 'Java', FechaAlta: 'Junior', Estatus: 'MABE'},
-  {Id: 11, Tarea: 'Cerrado', Creador: 'Capgemini', Destinatario: 'Java', FechaAlta: 'Junior', Estatus: 'MABE'},
-  {Id: 12, Tarea: 'Cerrado', Creador: 'Capgemini', Destinatario: 'Java', FechaAlta: 'Junior', Estatus: 'MABE'}
-];
-
-const ELEMENT_DATA2: Tarea[] = [
-  {Fecha: '26/06/2019', Creador: 'Sistema', Comentario: 'MABE eliminó "C#" y "LinQ" de la lista de "Skills requeridos"'},
-  {Fecha: '26/06/2019', Creador: 'MABE', Comentario: 'MABE agregó "Spring MVC" a la lista de "Skills esenciales"'},
-  {Fecha: '04/01/2019', Creador: 'Sistema', Comentario: 'El cliente me solicita esperar a que se lleven a cabo las entrevistas antes de enviar más gente.'},
-  {Fecha: '04/01/2019', Creador: 'Sistema', Comentario: 'MABE actualizó el campo "Tarifa" de $35 000.00 a $45 000.00'}
-];
-const ELEMENT_DATA3: Skills[] = [
-  {Skills: 'Hibernate', Dominio: 'Intermedio', Calificacion: '10.0', Eliminar: 'Eliminar'},
-  {Skills: 'Angular', Dominio: 'Avanzado', Calificacion: '9.0', Eliminar: 'Eliminar'},
-  {Skills: 'Java', Dominio: 'Principiante', Calificacion: '7.0', Eliminar: 'Eliminar'},
-];
 @Component({
   selector: 'jhi-res-concand',
   templateUrl: './res-concand.component.html',
@@ -60,50 +33,117 @@ const ELEMENT_DATA3: Skills[] = [
   ]
 })
 export class ResConcandComponent implements OnInit {
-// Inicio datatable
-@ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
-// Fin datatable
-// Inicio datatable
-@ViewChild(MatPaginator, {static: true}) paginator2: MatPaginator;
-// Fin datatable
-// Inicio datatable
-@ViewChild(MatPaginator, {static: true}) paginator3: MatPaginator;
-// Fin datatable
-// Inicio datatable
-  displayedColumns: string[] = ['Id', 'Tarea', 'Creador', 'Destinatario' , 'FechaAlta' , 'Estatus'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
-  @ViewChild(MatSort, {static: true}) sort: MatSort;
-// Fin datatable
-// Inicio datatable
-  displayedColumns2: string[] = ['Fecha', 'Creador', 'Comentario'];
-  dataSource2 = new MatTableDataSource(ELEMENT_DATA2);
-  @ViewChild(MatSort, {static: true}) sort2: MatSort;
-// Fin datatable
-// Inicio datatable
-  displayedColumns3: string[] = ['Skills', 'Dominio', 'Calificacion', 'Eliminar'];
-  dataSource3 = new MatTableDataSource(ELEMENT_DATA3);
-  @ViewChild(MatSort, {static: true}) sort3: MatSort;
-// Fin datatable
+
+  // Variables Tarea
+  ELEMENT_DATA2: Tarea[] = [
+    { Fecha: '26/06/2019', Creador: 'Sistema', Comentario: 'MABE eliminó "C#" y "LinQ" de la lista de "Skills requeridos"' },
+    { Fecha: '26/06/2019', Creador: 'MABE', Comentario: 'MABE agregó "Spring MVC" a la lista de "Skills esenciales"' },
+    { Fecha: '04/01/2019', Creador: 'Sistema', Comentario: 'El cliente me solicita esperar a que se lleven a cabo las entrevistas antes de enviar más gente.' },
+    { Fecha: '04/01/2019', Creador: 'Sistema', Comentario: 'MABE actualizó el campo "Tarifa" de $35 000.00 a $45 000.00' }
+  ];
+  dataSourceTarea: Tarea[];
+  displayedColumnsTarea: string[] = ['Fecha', 'Creador', 'Comentario'];
+
+  // Variables Bitacora
+  ELEMENT_DATA: PeriodicElement[] = [
+    { Id: 1, Tarea: 'Abierto', Creador: 'Capgemini', Destinatario: 'Java', FechaAlta: 'Junior', Estatus: 'MABE' },
+    { Id: 2, Tarea: 'Abierto', Creador: 'Capgemini', Destinatario: 'Java', FechaAlta: 'Junior', Estatus: 'MABE' },
+    { Id: 3, Tarea: 'Abierto', Creador: 'Capgemini', Destinatario: 'Java', FechaAlta: 'Junior', Estatus: 'MABE' },
+    { Id: 4, Tarea: 'Abierto', Creador: 'Capgemini', Destinatario: 'Java', FechaAlta: 'Junior', Estatus: 'MABE' },
+    { Id: 5, Tarea: 'Abierto', Creador: 'Capgemini', Destinatario: 'Java', FechaAlta: 'Junior', Estatus: 'MABE' },
+    { Id: 3, Tarea: 'Abierto', Creador: 'Capgemini', Destinatario: 'Java', FechaAlta: 'Junior', Estatus: 'MABE' },
+    { Id: 6, Tarea: 'Abierto', Creador: 'Capgemini', Destinatario: 'Java', FechaAlta: 'Junior', Estatus: 'MABE' },
+    { Id: 7, Tarea: 'Abierto', Creador: 'Capgemini', Destinatario: 'Java', FechaAlta: 'Junior', Estatus: 'MABE' },
+    { Id: 8, Tarea: 'Abierto', Creador: 'Capgemini', Destinatario: 'Java', FechaAlta: 'Junior', Estatus: 'MABE' },
+    { Id: 9, Tarea: 'Cerrado', Creador: 'Capgemini', Destinatario: 'Java', FechaAlta: 'Junior', Estatus: 'MABE' },
+    { Id: 10, Tarea: 'Cerrado', Creador: 'Capgemini', Destinatario: 'Java', FechaAlta: 'Junior', Estatus: 'MABE' },
+    { Id: 11, Tarea: 'Cerrado', Creador: 'Capgemini', Destinatario: 'Java', FechaAlta: 'Junior', Estatus: 'MABE' },
+    { Id: 12, Tarea: 'Cerrado', Creador: 'Capgemini', Destinatario: 'Java', FechaAlta: 'Junior', Estatus: 'MABE' }
+  ];
+  dataSourceBitacora: PeriodicElement[];
+  displayedColumnsBitacora: string[] = ['Id', 'Tarea', 'Creador', 'Destinatario', 'FechaAlta', 'Estatus'];
+
+  // Variables Skills
+  ELEMENT_DATA3: Skills[] = [
+    { Skills: 'Hibernate', Dominio: 'Intermedio', Calificacion: '10.0', Eliminar: 'Eliminar' },
+    { Skills: 'Angular', Dominio: 'Avanzado', Calificacion: '9.0', Eliminar: 'Eliminar' },
+    { Skills: 'Java', Dominio: 'Principiante', Calificacion: '7.0', Eliminar: 'Eliminar' },
+  ];
+  dataSourceSkills: Skills[];
+  displayedColumnsSkills: string[] = ['Skills', 'Dominio', 'Calificacion', 'Eliminar'];
 
   message: string;
 
   constructor() {
     this.message = 'ConCandComponent message';
+    this.dataSourceTarea = this.ELEMENT_DATA2.slice();
+    this.dataSourceBitacora = this.ELEMENT_DATA.slice();
+    this.dataSourceSkills = this.ELEMENT_DATA3.slice();
+  }
+
+  sortData(sort: MatSort) {
+    const data = this.ELEMENT_DATA2.slice();
+    if (!sort.active || sort.direction === '') {
+      this.dataSourceTarea = data;
+      return;
+    }
+
+    this.dataSourceTarea = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'Fecha': return compare(a.Fecha, b.Fecha, isAsc);
+        case 'Creador': return compare(a.Creador, b.Creador, isAsc);
+        case 'Comentario': return compare(a.Comentario, b.Comentario, isAsc);
+        default: return 0;
+      }
+    });
+  }
+
+  sortDataBitacora(sort: MatSort) {
+    const data = this.ELEMENT_DATA.slice();
+    if (!sort.active || sort.direction === '') {
+      this.dataSourceBitacora = data;
+      return;
+    }
+
+    this.dataSourceBitacora = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'Id': return compare(a.Id, b.Id, isAsc);
+        case 'Tarea': return compare(a.Tarea, b.Tarea, isAsc);
+        case 'Creador': return compare(a.Creador, b.Creador, isAsc);
+        case 'Destinatario': return compare(a.Destinatario, b.Destinatario, isAsc);
+        case 'FechaAlta': return compare(a.FechaAlta, b.FechaAlta, isAsc);
+        case 'Estatus': return compare(a.Estatus, b.Estatus, isAsc);
+        default: return 0;
+      }
+    });
+  }
+
+  sortDataSkills(sort: MatSort) {
+    const data = this.ELEMENT_DATA3.slice();
+    if (!sort.active || sort.direction === '') {
+      this.dataSourceSkills = data;
+      return;
+    }
+
+    this.dataSourceSkills = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'Skills': return compare(a.Skills, b.Skills, isAsc);
+        case 'Dominio': return compare(a.Dominio, b.Dominio, isAsc);
+        case 'Calificacion': return compare(a.Calificacion, b.Calificacion, isAsc);
+        case 'Eliminar': return compare(a.Eliminar, b.Eliminar, isAsc);
+        default: return 0;
+      }
+    });
   }
 
   ngOnInit() {
-    // Inicio datatable
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
-    // Fin datatable
-    // Inicio datatable
-    this.dataSource2.paginator = this.paginator2;
-    this.dataSource2.sort = this.sort2;
-    // Fin datatable
-    // Inicio datatable
-    this.dataSource3.paginator = this.paginator3;
-    this.dataSource3.sort = this.sort3;
-    // Fin datatable
   }
 
+}
+
+function compare(a: number | string, b: number | string, isAsc: boolean) {
+  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }
