d78b8bd6ae15abd65cf180a53e4cfaee394943a5
svnuser4 <brandon.gonzalez@kode.com.mx>
2019-07-09 16:24:00 +0000

    Pantallas terminadas y agregadas

--- src/main/webapp/app/con-req/con-req.component.ts
+++ src/main/webapp/app/con-req/con-req.component.ts
@@ -1,4 +1,40 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+// Inicio datatable
+import {MatSort} from '@angular/material/sort';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
+
+export interface PeriodicElement {
+  Id: number;
+  Estatus: string;
+  Cuenta: string;
+  Perfil: string;
+  Nivel: string;
+  Ejecutivo: string;
+  Reclutador: string;
+  TipoSolic: string;
+  VacSolic: string;
+  VacPend: string;
+  UltEnvCv: string;
+  UltEnt: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {Id: 1, Estatus: 'Abierto', Cuenta: 'Capgemini', Perfil: 'Java', Nivel: 'Junior', Ejecutivo: 'MABE' , Reclutador: 'JCE', TipoSolic: 'OutSourcing', VacSolic: '3', VacPend: '2', UltEnvCv: '01-Enero-2019', UltEnt: '24-Enero-2019'},
+  {Id: 2, Estatus: 'Abierto', Cuenta: 'Capgemini', Perfil: 'Java', Nivel: 'Semi Sr', Ejecutivo: 'MABE' , Reclutador: 'JCE', TipoSolic: 'OutSourcing', VacSolic: '3', VacPend: '2', UltEnvCv: '01-Enero-2019', UltEnt: '24-Enero-2019'},
+  {Id: 3, Estatus: 'Abierto', Cuenta: 'Capgemini', Perfil: 'Java', Nivel: 'Semi Sr', Ejecutivo: 'MABE' , Reclutador: 'JCE', TipoSolic: 'OutSourcing', VacSolic: '3', VacPend: '2', UltEnvCv: '01-Enero-2019', UltEnt: '24-Septiembre-2018'},
+  {Id: 4, Estatus: 'Abierto', Cuenta: 'Capgemini', Perfil: 'Java', Nivel: 'Semi Sr', Ejecutivo: 'MABE' , Reclutador: 'JCE', TipoSolic: 'OutSourcing', VacSolic: '3', VacPend: '2', UltEnvCv: '01-Enero-2019', UltEnt: '24-Enero-2019'},
+  {Id: 4, Estatus: 'Abierto', Cuenta: 'Capgemini', Perfil: 'Java', Nivel: 'Semi Sr', Ejecutivo: 'MABE' , Reclutador: 'JCE', TipoSolic: 'OutSourcing', VacSolic: '3', VacPend: '2', UltEnvCv: '01-Enero-2019', UltEnt: '24-Enero-2019'},
+  {Id: 4, Estatus: 'Cerrado', Cuenta: 'Capgemini', Perfil: 'Java', Nivel: 'Semi Sr', Ejecutivo: 'MABE' , Reclutador: 'FJR', TipoSolic: 'OutSourcing', VacSolic: '3', VacPend: '2', UltEnvCv: '01-Enero-2019', UltEnt: '24-Noviembre-2018'},
+  {Id: 4, Estatus: 'Cerrado', Cuenta: 'Capgemini', Perfil: 'Java', Nivel: 'Semi Sr', Ejecutivo: 'MABE' , Reclutador: 'FJR', TipoSolic: 'OutSourcing', VacSolic: '3', VacPend: '2', UltEnvCv: '01-Enero-2019', UltEnt: '24-Enero-2019'},
+  {Id: 4, Estatus: 'Cerrado', Cuenta: 'Capgemini', Perfil: 'Java', Nivel: 'Sr', Ejecutivo: 'CALS' , Reclutador: 'FJR', TipoSolic: 'Interno', VacSolic: '1', VacPend: '2', UltEnvCv: '05-Mayo-2019', UltEnt: '24-Marzo-2019'},
+  {Id: 9, Estatus: 'Cerrado', Cuenta: 'Capgemini', Perfil: 'Java', Nivel: 'Sr', Ejecutivo: 'CALS' , Reclutador: 'FJR', TipoSolic: 'Interno', VacSolic: '1', VacPend: '2', UltEnvCv: '05-Mayo-2019', UltEnt: '24-Marzo-2019'},
+  {Id: 9, Estatus: 'Cerrado', Cuenta: 'Capgemini', Perfil: 'Java', Nivel: 'Sr', Ejecutivo: 'CALS' , Reclutador: 'PCG', TipoSolic: 'Interno', VacSolic: '1', VacPend: '0', UltEnvCv: '05-Mayo-2019', UltEnt: '24-Marzo-2019'},
+  {Id: 9, Estatus: 'Cerrado', Cuenta: 'Capgemini', Perfil: 'Java', Nivel: 'Sr', Ejecutivo: 'CALS' , Reclutador: 'PCG', TipoSolic: 'Interno', VacSolic: '1', VacPend: '1', UltEnvCv: '05-Mayo-2019', UltEnt: '24-Marzo-2019'},
+  {Id: 9, Estatus: 'Cerrado', Cuenta: 'Capgemini', Perfil: 'Php', Nivel: 'Sr', Ejecutivo: 'CALS' , Reclutador: 'PCG', TipoSolic: 'Interno', VacSolic: '1', VacPend: '1', UltEnvCv: '05-Mayo-2019', UltEnt: '24-Enero-2019'},
+  {Id: 9, Estatus: 'Cerrado', Cuenta: 'Capgemini', Perfil: 'Php', Nivel: 'Sr', Ejecutivo: 'CALS' , Reclutador: 'PCG', TipoSolic: 'Interno', VacSolic: '1', VacPend: '1', UltEnvCv: '05-Mayo-2019', UltEnt: '24-Abril-2019'}
+];
+// Fin datatable
 
 @Component({
   selector: 'jhi-con-req',
@@ -7,8 +43,19 @@ import {Component, OnInit} from '@angular/core';
     'con-req.component.scss'
   ]
 })
+
 export class ConReqComponent implements OnInit {
 
+  // Inicio datatable
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  // Fin datatable
+
+  // Inicio datatable
+  displayedColumns: string[] = ['Id', 'Estatus', 'Cuenta', 'Perfil', 'Nivel', 'Ejecutivo', 'Reclutador', 'TipoSolic', 'VacSolic', 'VacPend', 'UltEnvCv', 'UltEnt'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+  // Fin datatable
   message: string;
 
   constructor() {
@@ -16,6 +63,10 @@ export class ConReqComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.dataSource.paginator = this.paginator;
+    // Inicio datatable
+    this.dataSource.sort = this.sort;
+    // Fin datatable
   }
 
 }
