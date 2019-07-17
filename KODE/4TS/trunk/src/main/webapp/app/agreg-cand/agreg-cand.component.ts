708e19376d930a27b7d4b6096807b39f01bcbed1
svnuser4 <svnuser4@07dfc717-5244-0410-b0d6-a68f8053c131>
2019-07-17 18:14:32 +0000

    mostrar mapa en agreg-cand.component.html

--- KODE/4TS/trunk/src/main/webapp/app/agreg-cand/agreg-cand.component.ts
+++ KODE/4TS/trunk/src/main/webapp/app/agreg-cand/agreg-cand.component.ts
@@ -33,6 +33,11 @@ const ELEMENT_DATA2: Tarea[] = [
 })
 export class AgregCandComponent implements OnInit {
 
+  // Enfoque del mapa
+  lat = 19.4284700;
+  lng = -99.1276600;
+  zoom = 12;
+
 // Inicio datatable
 @ViewChild(MatPaginator, {static: true}) paginator2: MatPaginator;
 // Fin datatable
