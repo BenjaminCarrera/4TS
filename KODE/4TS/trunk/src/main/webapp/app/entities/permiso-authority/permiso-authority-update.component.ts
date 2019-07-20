362a72562030cd4ab06da0e63b76d3f1b5c4faa9
svnuser4 <svnuser4@07dfc717-5244-0410-b0d6-a68f8053c131>
2019-07-20 00:12:23 +0000

    funcionalidad permiso-authority

--- KODE/4TS/trunk/src/main/webapp/app/entities/permiso-authority/permiso-authority-update.component.ts
+++ KODE/4TS/trunk/src/main/webapp/app/entities/permiso-authority/permiso-authority-update.component.ts
@@ -114,6 +114,7 @@ export class PermisoAuthorityUpdateComponent implements OnInit {
   protected onSaveError() {
     this.isSaving = false;
   }
+
   protected onError(errorMessage: string) {
     this.jhiAlertService.error(errorMessage, null, null);
   }
