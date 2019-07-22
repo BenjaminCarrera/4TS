08dd68c85a6f84d00e18e9000e780cf414b9c6a2
svnuser4 <brandon.gonzalez@kode.com.mx>
2019-07-22 21:33:27 +0000

    Requerimiento funcionando.

--- src/main/webapp/app/entities/requerimiento/requerimiento-detail.component.ts
+++ src/main/webapp/app/entities/requerimiento/requerimiento-detail.component.ts
@@ -5,17 +5,27 @@ import { IRequerimiento } from 'app/shared/model/requerimiento.model';
 
 @Component({
   selector: 'jhi-requerimiento-detail',
-  templateUrl: './requerimiento-detail.component.html'
+  templateUrl: './requerimiento-detail.component.html',
+  styleUrls: [
+    '../../res-conreq/res-conreq.component.scss'
+  ]
 })
 export class RequerimientoDetailComponent implements OnInit {
+  // Enfoque del mapa
+  lat: any;
+  lng: any;
+  zoom = 10;
   requerimiento: IRequerimiento;
-
   constructor(protected activatedRoute: ActivatedRoute) {}
 
   ngOnInit() {
     this.activatedRoute.data.subscribe(({ requerimiento }) => {
       this.requerimiento = requerimiento;
     });
+    // Enfoque del mapa
+    this.lat = this.requerimiento.coorLat;
+    this.lng = this.requerimiento.coorLong;
+    this.zoom = 10;
   }
 
   previousState() {
