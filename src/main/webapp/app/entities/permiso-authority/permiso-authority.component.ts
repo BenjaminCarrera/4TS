e0f48272417431e22fababc644fdfbc62261afb8
svnuser4 <brandon.gonzalez@kode.com.mx>
2019-07-23 19:19:39 +0000

    funcionalidad en la tabla de permiso-authority.component.html

--- src/main/webapp/app/entities/permiso-authority/permiso-authority.component.ts
+++ src/main/webapp/app/entities/permiso-authority/permiso-authority.component.ts
@@ -14,17 +14,19 @@ import { PermisoAuthorityService } from './permiso-authority.service';
 import { PermisoAuthority } from '../../shared/model/permiso-authority.model';
 import { IPermiso } from '../../shared/model/permiso.model';
 import { IArrePermisoAuthority, ArrePermisoAuthority } from '../../shared/model/permiso-authority-array.model';
+import { adminState } from '../../admin/admin.route';
+import { permisoAuthorityRoute } from './permiso-authority.route';
 
 @Component({
   selector: 'jhi-permiso-authority',
   templateUrl: './permiso-authority.component.html'
 })
 export class PermisoAuthorityComponent implements OnInit, OnDestroy {
-
   isSaving: boolean;
 
   currentAccount: any;
   permisoAuthorities: IPermisoAuthority[];
+  permisoAuthority: IPermisoAuthority;
   error: any;
   success: any;
   eventSubscriber: Subscription;
@@ -37,17 +39,18 @@ export class PermisoAuthorityComponent implements OnInit, OnDestroy {
   previousPage: any;
   reverse: any;
 
-  temp: IArrePermisoAuthority;  newArre: any[] = [];
-  editForm = this.fb.group({
-    idAdmin: [],
-    estatusAdmin: [],
-    idReq: [],
-    estatusReq: [],
-    idCand: [],
-    estatusCand: [],
-    idEntity: [],
-    estatusEntity: []
-  });
+  temp: IArrePermisoAuthority;
+  newArre: any[];
+  // editForm = this.fb.group({
+  //   idAdmin: [],
+  //   estatusAdmin: [],
+  //   idReq: [],
+  //   estatusReq: [],
+  //   idCand: [],
+  //   estatusCand: [],
+  //   idEntity: [],
+  //   estatusEntity: []
+  // });
 
   constructor(
     protected permisoAuthorityService: PermisoAuthorityService,
@@ -141,58 +144,106 @@ export class PermisoAuthorityComponent implements OnInit, OnDestroy {
 
   protected paginatePermisoAuthorities(data: IPermisoAuthority[], headers: HttpHeaders) {
     this.links = this.parseLinks.parse(headers.get('link'));
-    // this.totalItems = parseInt(headers.get('X-Total-Count'), 10);
+    this.totalItems = parseInt(headers.get('X-Total-Count'), 10);
+    this.newArre = [];
     this.permisoAuthorities = data;
 
     this.temp = new ArrePermisoAuthority();
     let contadorPermisos: number;
     contadorPermisos = 0;
-      this.permisoAuthorities.forEach(e => {
-        if (e.permisoId === 1) {
-          this.temp.admin = e.id;
-          this.temp.actAdmin = e.activated;
-        } else if (e.permisoId === 2) {
-          this.temp.requirements = e.id;
-          this.temp.actRequirements = e.activated;
-        } else if (e.permisoId === 3) {
-          this.temp.candidates = e.id;
-          this.temp.actCandidates = e.activated;
-        } else if (e.permisoId === 4) {
-          this.temp.entity = e.id;
-          this.temp.actEntity = e.activated;
-          this.temp.authority = e.authority;
-          this.newArre.push(this.temp);
-          contadorPermisos = contadorPermisos + 1;
-          this.temp = new ArrePermisoAuthority();
-        }
-        });
-        this.totalItems = contadorPermisos;
-
+    this.permisoAuthorities.forEach(e => {
+      if (e.permisoId === 1) {
+        this.temp.admin = e.id;
+        this.temp.actAdmin = e.activated;
+      } else if (e.permisoId === 2) {
+        this.temp.requirements = e.id;
+        this.temp.actRequirements = e.activated;
+      } else if (e.permisoId === 3) {
+        this.temp.candidates = e.id;
+        this.temp.actCandidates = e.activated;
+      } else if (e.permisoId === 4) {
+        this.temp.entity = e.id;
+        this.temp.actEntity = e.activated;
+        this.temp.authority = e.authority;
+        this.newArre.push(this.temp);
+        contadorPermisos = contadorPermisos + 1;
+        this.temp = new ArrePermisoAuthority();
+      }
+    });
+    this.totalItems = contadorPermisos;
   }
 
   protected onError(errorMessage: string) {
     this.jhiAlertService.error(errorMessage, null, null);
   }
 
-  save() {
-    this.editForm.get('');
+  save(permisos: any) {
+    console.log(permisos);
+    this.permisoAuthorities.forEach(e => {
+      this.permisoAuthority = {};
+      this.permisoAuthority.id = e.id;
+      this.permisoAuthority.authority = e.authority;
+      this.permisoAuthority.createdBy = e.createdBy;
+      this.permisoAuthority.deleted = e.deleted;
+      this.permisoAuthority.lastModifiedBy = e.lastModifiedBy;
+      this.permisoAuthority.lastModifiedDate = e.lastModifiedDate;
+      this.permisoAuthority.permisoId = e.permisoId;
+      this.permisoAuthority.permisoNombre = e.permisoNombre;
+      if (this.permisoAuthority.id === permisos.admin) {
+        e.activated = permisos.actAdmin;
+        this.permisoAuthority.activated = e.activated;
+        // this.permisoAuthority.id = permisos.admin;
+        // this.permisoAuthority.activated = permisos.actAdmin;
+        this.permisoAuthorityService
+          .update(
+            this.permisoAuthority
+          )
+          .subscribe(
+            (res: HttpResponse<IPermisoAuthority>) => console.log(res),
+            (res: HttpErrorResponse) => this.onError(res.message)
+          );
+      } else if (this.permisoAuthority.id === permisos.requirements) {
+        e.activated = permisos.actRequirements;
+        this.permisoAuthority.activated = e.activated;
+      //   this.permisoAuthority.id = permisos.requirements;
+      //   this.permisoAuthority.activated = permisos.actRequirements;
+        this.permisoAuthorityService
+        .update(
+          this.permisoAuthority
+        )
+        .subscribe(
+          (res: HttpResponse<IPermisoAuthority>) => console.log(res),
+          (res: HttpErrorResponse) => this.onError(res.message)
+        );
+      } else if (this.permisoAuthority.id === permisos.candidates) {
+        e.activated = permisos.actCandidates;
+        this.permisoAuthority.activated = e.activated;
+        // this.permisoAuthority.id = permisos.candidates;
+        // this.permisoAuthority.activated = permisos.actCandidates;
+        this.permisoAuthorityService
+        .update(
+          this.permisoAuthority
+        )
+        .subscribe(
+          (res: HttpResponse<IPermisoAuthority>) => console.log(res),
+          (res: HttpErrorResponse) => this.onError(res.message)
+        );
+      } else if (this.permisoAuthority.id === permisos.entity) {
+        e.activated = permisos.actEntity;
+        this.permisoAuthority.activated = e.activated;
+        // this.permisoAuthority.id = permisos.entity;
+        // this.permisoAuthority.activated = permisos.actEntity;
+        this.permisoAuthorityService
+        .update(
+          this.permisoAuthority
+        )
+        .subscribe(
+          (res: HttpResponse<IPermisoAuthority>) => console.log(res),
+          (res: HttpErrorResponse) => this.onError(res.message)
+        );
+      }
+    });
+    this.clear();
+    console.log(this.permisoAuthorities);
   }
-
-  protected subscribeToSaveResponse(result: Observable<HttpResponse<IPermisoAuthority>>) {
-    result.subscribe(() => this.onSaveSuccess(), () => this.onSaveError());
-  }
-
-  protected onSaveSuccess() {
-    this.isSaving = false;
-    this.previousState();
-  }
-
-  previousState() {
-    window.history.back();
-  }
-
-  protected onSaveError() {
-    this.isSaving = false;
-  }
-
 }
