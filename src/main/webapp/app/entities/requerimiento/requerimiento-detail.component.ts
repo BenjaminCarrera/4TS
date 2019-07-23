f26ffa32a0571eebb8e3e54ee2692eae05ac75a6
svnuser4 <brandon.gonzalez@kode.com.mx>
2019-07-23 00:00:09 +0000

    Añadido boton de editar, tablas de bitacora y tareas.

--- src/main/webapp/app/entities/requerimiento/requerimiento-detail.component.ts
+++ src/main/webapp/app/entities/requerimiento/requerimiento-detail.component.ts
@@ -1,7 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
+import { MatSort } from '@angular/material/sort';
 import { IRequerimiento } from 'app/shared/model/requerimiento.model';
+
+export interface PeriodicElement {
+  Id: number;
+  Tarea: string;
+  Creador: string;
+  Destinatario: string;
+  FechaAlta: string;
+  Estatus: string;
+}
+
+export interface Tarea {
+  Fecha: string;
+  Creador: string;
+  Comentario: string;
+}
 
 @Component({
   selector: 'jhi-requerimiento-detail',
@@ -11,13 +26,81 @@ import { IRequerimiento } from 'app/shared/model/requerimiento.model';
   ]
 })
 export class RequerimientoDetailComponent implements OnInit {
+  // Variables Tarea
+  DATA_TAREA: PeriodicElement[] = [
+    { Id: 1, Tarea: 'Abiertas', Creador: 'Capgemini', Destinatario: 'Java', FechaAlta: 'Junior', Estatus: 'MABE' },
+    { Id: 2, Tarea: 'Abiertas', Creador: 'Capgemini', Destinatario: 'Java', FechaAlta: 'Junior', Estatus: 'MABE' },
+    { Id: 3, Tarea: 'Abiertas', Creador: 'Capgemini', Destinatario: 'Java', FechaAlta: 'Junior', Estatus: 'MABE' },
+    { Id: 4, Tarea: 'Abiertas', Creador: 'Capgemini', Destinatario: 'Java', FechaAlta: 'Junior', Estatus: 'MABE' },
+    { Id: 5, Tarea: 'Abiertas', Creador: 'Capgemini', Destinatario: 'Java', FechaAlta: 'Junior', Estatus: 'MABE' },
+    { Id: 3, Tarea: 'Abiertas', Creador: 'Capgemini', Destinatario: 'Java', FechaAlta: 'Junior', Estatus: 'MABE' },
+    { Id: 6, Tarea: 'Abiertas', Creador: 'Capgemini', Destinatario: 'Java', FechaAlta: 'Junior', Estatus: 'MABE' },
+    { Id: 7, Tarea: 'Abiertas', Creador: 'Capgemini', Destinatario: 'Java', FechaAlta: 'Junior', Estatus: 'MABE' },
+    { Id: 8, Tarea: 'Abiertas', Creador: 'Capgemini', Destinatario: 'Java', FechaAlta: 'Junior', Estatus: 'MABE' },
+    { Id: 9, Tarea: 'Cerradas', Creador: 'Capgemini', Destinatario: 'Java', FechaAlta: 'Junior', Estatus: 'MABE' },
+    { Id: 10, Tarea: 'Cerradas', Creador: 'Capgemini', Destinatario: 'Java', FechaAlta: 'Junior', Estatus: 'MABE' },
+    { Id: 11, Tarea: 'Cerradas', Creador: 'Capgemini', Destinatario: 'Java', FechaAlta: 'Junior', Estatus: 'MABE' },
+    { Id: 12, Tarea: 'Cerradas', Creador: 'Capgemini', Destinatario: 'Java', FechaAlta: 'Junior', Estatus: 'MABE' }
+  ];
+  dataSourceTarea: PeriodicElement[];
+  displayedColumnsTarea: string[] = ['Id', 'Tarea', 'Creador', 'Destinatario', 'FechaAlta', 'Estatus'];
+
+  // VAriables Bitacora
+  DATA_BITACORA: Tarea[] = [
+    { Fecha: '26/06/2019', Creador: 'Sistema', Comentario: 'MABE eliminó "C#" y "LinQ" de la lista de "Skills requeridos"' },
+    { Fecha: '26/06/2019', Creador: 'MABE', Comentario: 'MABE agregó "Spring MVC" a la lista de "Skills esenciales"' },
+    { Fecha: '04/01/2019', Creador: 'Sistema', Comentario: 'El cliente me solicita esperar a que se lleven a cabo las entrevistas antes de enviar más gente.' },
+    { Fecha: '04/01/2019', Creador: 'Sistema', Comentario: 'MABE actualizó el campo "Tarifa" de $35 000.00 a $45 000.00' }
+  ];
+  dataSourceBitacora: Tarea[];
+  displayedColumnsBitacora: string[] = ['Fecha', 'Creador', 'Comentario'];
   // Enfoque del mapa
   lat: any;
   lng: any;
   zoom = 10;
   requerimiento: IRequerimiento;
-  constructor(protected activatedRoute: ActivatedRoute) {}
+  constructor(protected activatedRoute: ActivatedRoute) {
+    this.dataSourceTarea = this.DATA_TAREA.slice();
+    this.dataSourceBitacora = this.DATA_BITACORA.slice();
+  }
 
+  sortDataTarea(sort: MatSort) {
+    const data = this.DATA_TAREA.slice();
+    if (!sort.active || sort.direction === '') {
+      this.dataSourceTarea = data;
+      return;
+    }
+
+    this.dataSourceTarea = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'Id': return compare(a.Id, b.Id, isAsc);
+        case 'Tarea': return compare(a.Tarea, b.Tarea, isAsc);
+        case 'Creador': return compare(a.Creador, b.Creador, isAsc);
+        case 'Destinatario': return compare(a.Destinatario, b.Destinatario, isAsc);
+        case 'FechaAlta': return compare(a.FechaAlta, b.FechaAlta, isAsc);
+        case 'Estatus': return compare(a.Estatus, b.Estatus, isAsc);
+        default: return 0;
+      }
+    });
+  }
+  sortDataBitacora(sort: MatSort) {
+    const data = this.DATA_BITACORA.slice();
+    if (!sort.active || sort.direction === '') {
+      this.dataSourceBitacora = data;
+      return;
+    }
+
+    this.dataSourceBitacora = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'Fecha': return compare(a.Fecha, b.Fecha, isAsc);
+        case 'Creador': return compare(a.Creador, b.Creador, isAsc);
+        case 'Comentario': return compare(a.Comentario, b.Comentario, isAsc);
+        default: return 0;
+      }
+    });
+  }
   ngOnInit() {
     this.activatedRoute.data.subscribe(({ requerimiento }) => {
       this.requerimiento = requerimiento;
@@ -31,4 +114,7 @@ export class RequerimientoDetailComponent implements OnInit {
   previousState() {
     window.history.back();
   }
+}
+function compare(a: number | string, b: number | string, isAsc: boolean) {
+  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }
