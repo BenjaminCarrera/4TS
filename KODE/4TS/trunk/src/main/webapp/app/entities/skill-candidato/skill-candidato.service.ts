374b607dcfc52138cf64161e2a6ec9e22a46f9f1
svnuser2 <svnuser2@07dfc717-5244-0410-b0d6-a68f8053c131>
2019-08-05 21:09:04 +0000

    Corregir fecha en candidato

--- KODE/4TS/trunk/src/main/webapp/app/entities/skill-candidato/skill-candidato.service.ts
+++ KODE/4TS/trunk/src/main/webapp/app/entities/skill-candidato/skill-candidato.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpResponse } from '@angular/common/http';
+import { HttpClient, HttpResponse, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { SERVER_API_URL } from 'app/app.constants';
@@ -12,6 +12,12 @@ type EntityArrayResponseType = HttpResponse<ISkillCandidato[]>;
 @Injectable({ providedIn: 'root' })
 export class SkillCandidatoService {
   public resourceUrl = SERVER_API_URL + 'api/skill-candidatoes';
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
 
   constructor(protected http: HttpClient) {}
 
@@ -34,5 +40,9 @@ export class SkillCandidatoService {
 
   delete(id: number): Observable<HttpResponse<any>> {
     return this.http.delete<any>(`${this.resourceUrl}/${id}`, { observe: 'response' });
+  }
+
+  patch(req: any): Observable<EntityArrayResponseType> {
+    return this.http.patch<any>(this.resourceUrl, JSON.stringify(req), this.httpOptions);
   }
 }
