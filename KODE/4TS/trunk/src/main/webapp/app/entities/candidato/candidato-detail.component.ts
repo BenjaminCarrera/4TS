2ae5b9c3cf941081635f12b18c9aeb8be744669d
svnuser4 <svnuser4@07dfc717-5244-0410-b0d6-a68f8053c131>
2019-08-07 01:06:52 +0000

    tablas de candidato-detail.component.html ya se llenan con la BD

--- KODE/4TS/trunk/src/main/webapp/app/entities/candidato/candidato-detail.component.ts
+++ KODE/4TS/trunk/src/main/webapp/app/entities/candidato/candidato-detail.component.ts
@@ -5,6 +5,16 @@ import { ICandidato } from 'app/shared/model/candidato.model';
 import { MatSort } from '@angular/material/sort';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import { ITarea } from '../../shared/model/tarea.model';
+import { TareaService } from '../tarea/tarea.service';
+import { HttpResponse, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { JhiAlertService } from 'ng-jhipster';
+import { IBitacora } from 'app/shared/model/bitacora.model';
+import { BitacoraService } from '../bitacora/bitacora.service';
+import { ISkill } from '../../shared/model/skill.model';
+import { SkillService } from '../skill/skill.service';
+import { ISkillCandidato } from '../../shared/model/skill-candidato.model';
+import { SkillCandidatoService } from '../skill-candidato';
 
 export interface PeriodicElement {
   Id: number;
@@ -30,9 +40,7 @@ export interface Skills {
 @Component({
   selector: 'jhi-candidato-detail',
   templateUrl: './candidato-detail.component.html',
-  styleUrls: [
-    '../../res-concand/res-concand.component.scss'
-  ]
+  styleUrls: ['../../res-concand/res-concand.component.scss']
 })
 export class CandidatoDetailComponent implements OnInit {
   // Mostrar u ocultar cosas
@@ -40,6 +48,12 @@ export class CandidatoDetailComponent implements OnInit {
   mostrarDisponibilidadEntrevistaCandidato: boolean;
   candidato: ICandidato;
   mostrarAsignacionCandidato: boolean;
+  tareas: ITarea[];
+  tareaCand: ITarea[];
+  bitacoras: IBitacora[];
+  bitacorasCand: IBitacora[];
+  skills: ISkillCandidato[];
+  skillsCand: ISkillCandidato[];
 
   // Enfoque del mapa
   lat = 19.391213;
@@ -47,115 +61,174 @@ export class CandidatoDetailComponent implements OnInit {
   zoom = 20;
 
   // Variables Tarea
-  DATA_TAREA: PeriodicElement[] = [
-    { Id: 1, Tarea: 'Abierto', Creador: 'Yasser', Destinatario: 'Juan', FechaAlta: '09/07/2019', Estatus: 'Abierta' },
-    { Id: 2, Tarea: 'Abierto', Creador: 'Yasser', Destinatario: 'Juan', FechaAlta: '09/07/2019', Estatus: 'Abierta' },
-    { Id: 3, Tarea: 'Abierto', Creador: 'Yasser', Destinatario: 'Juan', FechaAlta: '09/07/2019', Estatus: 'Abierta' },
-    { Id: 4, Tarea: 'Abierto', Creador: 'Yasser', Destinatario: 'Juan', FechaAlta: '09/07/2019', Estatus: 'Abierta' },
-    { Id: 5, Tarea: 'Abierto', Creador: 'Yasser', Destinatario: 'Juan', FechaAlta: '09/07/2019', Estatus: 'Abierta' },
-    { Id: 3, Tarea: 'Abierto', Creador: 'Yasser', Destinatario: 'Juan', FechaAlta: '09/07/2019', Estatus: 'Atendida' },
-    { Id: 6, Tarea: 'Abierto', Creador: 'Yasser', Destinatario: 'Juan', FechaAlta: '01/07/2019', Estatus: 'Abierta' },
-    { Id: 7, Tarea: 'Abierto', Creador: 'Yasser', Destinatario: 'Juan', FechaAlta: '01/07/2019', Estatus: 'Abierta' },
-    { Id: 8, Tarea: 'Abierto', Creador: 'Yasser', Destinatario: 'Juan', FechaAlta: '01/07/2019', Estatus: 'Cerrada' },
-    { Id: 9, Tarea: 'Cerrado', Creador: 'Yasser', Destinatario: 'Juan', FechaAlta: '01/07/2019', Estatus: 'Cerrada' },
-    { Id: 10, Tarea: 'Cerrado', Creador: 'Yasser', Destinatario: 'Juan', FechaAlta: '01/07/2019', Estatus: 'Cerrada' },
-    { Id: 11, Tarea: 'Cerrado', Creador: 'Yasser', Destinatario: 'Juan', FechaAlta: '01/07/2019', Estatus: 'Cerrada' },
-    { Id: 12, Tarea: 'Cerrado', Creador: 'Yasser', Destinatario: 'Juan', FechaAlta: '01/07/2019', Estatus: 'Cerrada' }
-  ];
-  dataSourceTarea: PeriodicElement[];
-  displayedColumnsTarea: string[] = ['Id', 'Tarea', 'Creador', 'Destinatario', 'FechaAlta', 'Estatus'];
+  // DATA_TAREA: PeriodicElement[] = [
+  //   { Id: 1, Tarea: 'Abierto', Creador: 'Yasser', Destinatario: 'Juan', FechaAlta: '09/07/2019', Estatus: 'Abierta' },
+  //   { Id: 2, Tarea: 'Abierto', Creador: 'Yasser', Destinatario: 'Juan', FechaAlta: '09/07/2019', Estatus: 'Abierta' },
+  //   { Id: 3, Tarea: 'Abierto', Creador: 'Yasser', Destinatario: 'Juan', FechaAlta: '09/07/2019', Estatus: 'Abierta' },
+  //   { Id: 4, Tarea: 'Abierto', Creador: 'Yasser', Destinatario: 'Juan', FechaAlta: '09/07/2019', Estatus: 'Abierta' },
+  //   { Id: 5, Tarea: 'Abierto', Creador: 'Yasser', Destinatario: 'Juan', FechaAlta: '09/07/2019', Estatus: 'Abierta' },
+  //   { Id: 3, Tarea: 'Abierto', Creador: 'Yasser', Destinatario: 'Juan', FechaAlta: '09/07/2019', Estatus: 'Atendida' },
+  //   { Id: 6, Tarea: 'Abierto', Creador: 'Yasser', Destinatario: 'Juan', FechaAlta: '01/07/2019', Estatus: 'Abierta' },
+  //   { Id: 7, Tarea: 'Abierto', Creador: 'Yasser', Destinatario: 'Juan', FechaAlta: '01/07/2019', Estatus: 'Abierta' },
+  //   { Id: 8, Tarea: 'Abierto', Creador: 'Yasser', Destinatario: 'Juan', FechaAlta: '01/07/2019', Estatus: 'Cerrada' },
+  //   { Id: 9, Tarea: 'Cerrado', Creador: 'Yasser', Destinatario: 'Juan', FechaAlta: '01/07/2019', Estatus: 'Cerrada' },
+  //   { Id: 10, Tarea: 'Cerrado', Creador: 'Yasser', Destinatario: 'Juan', FechaAlta: '01/07/2019', Estatus: 'Cerrada' },
+  //   { Id: 11, Tarea: 'Cerrado', Creador: 'Yasser', Destinatario: 'Juan', FechaAlta: '01/07/2019', Estatus: 'Cerrada' },
+  //   { Id: 12, Tarea: 'Cerrado', Creador: 'Yasser', Destinatario: 'Juan', FechaAlta: '01/07/2019', Estatus: 'Cerrada' }
+  // ];
+  // dataSourceTarea: PeriodicElement[];
+  // displayedColumnsTarea: string[] = ['Id', 'Tarea', 'Creador', 'Destinatario', 'FechaAlta', 'Estatus'];
 
   // Variables Bitacora
-  DATA_BITACORA: Tarea[] = [
-    { Fecha: '26/06/2019', Creador: 'Sistema', Comentario: 'MABE eliminó "C#" y "LinQ" de la lista de "Skills requeridos"' },
-    { Fecha: '26/06/2019', Creador: 'MABE', Comentario: 'MABE agregó "Spring MVC" a la lista de "Skills esenciales"' },
-    { Fecha: '04/01/2019', Creador: 'Sistema', Comentario: 'El cliente me solicita esperar a que se lleven a cabo las entrevistas antes de enviar más gente.' },
-    { Fecha: '04/01/2019', Creador: 'Sistema', Comentario: 'MABE actualizó el campo "Tarifa" de $35 000.00 a $45 000.00' }
-  ];
-  dataSourceBitacora: Tarea[];
-  displayedColumnsBitacora: string[] = ['Fecha', 'Creador', 'Comentario'];
+  // DATA_BITACORA: Tarea[] = [
+  //   { Fecha: '26/06/2019', Creador: 'Sistema', Comentario: 'MABE eliminó "C#" y "LinQ" de la lista de "Skills requeridos"' },
+  //   { Fecha: '26/06/2019', Creador: 'MABE', Comentario: 'MABE agregó "Spring MVC" a la lista de "Skills esenciales"' },
+  //   {
+  //     Fecha: '04/01/2019',
+  //     Creador: 'Sistema',
+  //     Comentario: 'El cliente me solicita esperar a que se lleven a cabo las entrevistas antes de enviar más gente.'
+  //   },
+  //   { Fecha: '04/01/2019', Creador: 'Sistema', Comentario: 'MABE actualizó el campo "Tarifa" de $35 000.00 a $45 000.00' }
+  // ];
+  // dataSourceBitacora: Tarea[];
+  // displayedColumnsBitacora: string[] = ['Fecha', 'Creador', 'Comentario'];
 
   // Variables Skills
-  DATA_SKILLS: Skills[] = [
-    { Skills: 'Hibernate', Dominio: 'Intermedio', Calificacion: '10.0', Eliminar: 'Eliminar' },
-    { Skills: 'Angular', Dominio: 'Avanzado', Calificacion: '9.0', Eliminar: 'Eliminar' },
-    { Skills: 'Java', Dominio: 'Principiante', Calificacion: '7.0', Eliminar: 'Eliminar' },
-  ];
-  dataSourceSkills: Skills[];
-  displayedColumnsSkills: string[] = ['Skills', 'Dominio', 'Calificacion'];
+  // DATA_SKILLS: Skills[] = [
+  //   { Skills: 'Hibernate', Dominio: 'Intermedio', Calificacion: '10.0', Eliminar: 'Eliminar' },
+  //   { Skills: 'Angular', Dominio: 'Avanzado', Calificacion: '9.0', Eliminar: 'Eliminar' },
+  //   { Skills: 'Java', Dominio: 'Principiante', Calificacion: '7.0', Eliminar: 'Eliminar' }
+  // ];
+  // dataSourceSkills: Skills[];
+  // displayedColumnsSkills: string[] = ['Skills', 'Dominio', 'Calificacion'];
 
   message: string;
+  page: number;
 
-  constructor(protected activatedRoute: ActivatedRoute) {
-    this.dataSourceTarea = this.DATA_TAREA.slice();
-    this.dataSourceBitacora = this.DATA_BITACORA.slice();
-    this.dataSourceSkills = this.DATA_SKILLS.slice();
+  constructor(
+    protected activatedRoute: ActivatedRoute,
+    protected jhiAlertService: JhiAlertService,
+    protected tareaService: TareaService,
+    protected bitacoraService: BitacoraService,
+    protected skillCandidatoService: SkillCandidatoService
+    ) {
+    // this.dataSourceTarea = this.DATA_TAREA.slice();
+    // this.dataSourceBitacora = this.DATA_BITACORA.slice();
+    // this.dataSourceSkills = this.DATA_SKILLS.slice();
   }
 
-  sortDataTarea(sort: MatSort) {
-    const data = this.DATA_TAREA.slice();
-    if (!sort.active || sort.direction === '') {
-      this.dataSourceTarea = data;
-      return;
-    }
+  // sortDataTarea(sort: MatSort) {
+  //   const data = this.DATA_TAREA.slice();
+  //   if (!sort.active || sort.direction === '') {
+  //     this.dataSourceTarea = data;
+  //     return;
+  //   }
 
-    this.dataSourceTarea = data.sort((a, b) => {
-      const isAsc = sort.direction === 'asc';
-      switch (sort.active) {
-        case 'Id': return compare(a.Id, b.Id, isAsc);
-        case 'Tarea': return compare(a.Tarea, b.Tarea, isAsc);
-        case 'Creador': return compare(a.Creador, b.Creador, isAsc);
-        case 'Destinatario': return compare(a.Destinatario, b.Destinatario, isAsc);
-        case 'FechaAlta': return compare(a.FechaAlta, b.FechaAlta, isAsc);
-        case 'Estatus': return compare(a.Estatus, b.Estatus, isAsc);
-        default: return 0;
-      }
-    });
-  }
+  //   this.dataSourceTarea = data.sort((a, b) => {
+  //     const isAsc = sort.direction === 'asc';
+  //     switch (sort.active) {
+  //       case 'Id':
+  //         return compare(a.Id, b.Id, isAsc);
+  //       case 'Tarea':
+  //         return compare(a.Tarea, b.Tarea, isAsc);
+  //       case 'Creador':
+  //         return compare(a.Creador, b.Creador, isAsc);
+  //       case 'Destinatario':
+  //         return compare(a.Destinatario, b.Destinatario, isAsc);
+  //       case 'FechaAlta':
+  //         return compare(a.FechaAlta, b.FechaAlta, isAsc);
+  //       case 'Estatus':
+  //         return compare(a.Estatus, b.Estatus, isAsc);
+  //       default:
+  //         return 0;
+  //     }
+  //   });
+  // }
 
-  sortDataBitacora(sort: MatSort) {
-    const data = this.DATA_BITACORA.slice();
-    if (!sort.active || sort.direction === '') {
-      this.dataSourceBitacora = data;
-      return;
-    }
+  // sortDataBitacora(sort: MatSort) {
+  //   const data = this.DATA_BITACORA.slice();
+  //   if (!sort.active || sort.direction === '') {
+  //     this.dataSourceBitacora = data;
+  //     return;
+  //   }
 
-    this.dataSourceBitacora = data.sort((a, b) => {
-      const isAsc = sort.direction === 'asc';
-      switch (sort.active) {
-        case 'Fecha': return compare(a.Fecha, b.Fecha, isAsc);
-        case 'Creador': return compare(a.Creador, b.Creador, isAsc);
-        case 'Comentario': return compare(a.Comentario, b.Comentario, isAsc);
-        default: return 0;
-      }
-    });
-  }
+  //   this.dataSourceBitacora = data.sort((a, b) => {
+  //     const isAsc = sort.direction === 'asc';
+  //     switch (sort.active) {
+  //       case 'Fecha':
+  //         return compare(a.Fecha, b.Fecha, isAsc);
+  //       case 'Creador':
+  //         return compare(a.Creador, b.Creador, isAsc);
+  //       case 'Comentario':
+  //         return compare(a.Comentario, b.Comentario, isAsc);
+  //       default:
+  //         return 0;
+  //     }
+  //   });
+  // }
 
-  sortDataSkills(sort: MatSort) {
-    const data = this.DATA_SKILLS.slice();
-    if (!sort.active || sort.direction === '') {
-      this.dataSourceSkills = data;
-      return;
-    }
+  // sortDataSkills(sort: MatSort) {
+  //   const data = this.DATA_SKILLS.slice();
+  //   if (!sort.active || sort.direction === '') {
+  //     this.dataSourceSkills = data;
+  //     return;
+  //   }
 
-    this.dataSourceSkills = data.sort((a, b) => {
-      const isAsc = sort.direction === 'asc';
-      switch (sort.active) {
-        case 'Skills': return compare(a.Skills, b.Skills, isAsc);
-        case 'Dominio': return compare(a.Dominio, b.Dominio, isAsc);
-        case 'Calificacion': return compare(a.Calificacion, b.Calificacion, isAsc);
-        default: return 0;
-      }
-    });
-  }
+  //   this.dataSourceSkills = data.sort((a, b) => {
+  //     const isAsc = sort.direction === 'asc';
+  //     switch (sort.active) {
+  //       case 'Skills':
+  //         return compare(a.Skills, b.Skills, isAsc);
+  //       case 'Dominio':
+  //         return compare(a.Dominio, b.Dominio, isAsc);
+  //       case 'Calificacion':
+  //         return compare(a.Calificacion, b.Calificacion, isAsc);
+  //       default:
+  //         return 0;
+  //     }
+  //   });
+  // }
   ngOnInit() {
     this.activatedRoute.data.subscribe(({ candidato }) => {
       this.candidato = candidato;
     });
+    this.loadAll();
     this.verificarStatus();
     this.verificarDisponibilidadEntrevista();
     this.verificarDisponibilidaAsignacion();
     this.candidato.foto = 'content/fotoCandidato/' + this.candidato.foto;
+  }
+
+  loadAll() {
+    this.tareaService
+      .query({
+        page: this.page - 1,
+        size: 9999
+      })
+      .subscribe(
+        (res: HttpResponse<ITarea[]>) => this.paginateTareas(res.body, res.headers),
+        (res: HttpErrorResponse) => this.onError(res.message)
+      );
+
+      this.bitacoraService
+      .query({
+        page: this.page - 1,
+        size: 9999
+      })
+      .subscribe(
+        (res: HttpResponse<IBitacora[]>) => this.paginateBitacoras(res.body, res.headers),
+        (res: HttpErrorResponse) => this.onError(res.message)
+      );
+
+      this.skillCandidatoService
+      .query({
+        page: this.page - 1,
+        size: 9999
+      })
+      .subscribe(
+        (res: HttpResponse<ISkillCandidato[]>) => this.paginateSkillCandidatoes(res.body, res.headers),
+        (res: HttpErrorResponse) => this.onError(res.message)
+      );
   }
 
   previousState() {
@@ -190,6 +263,34 @@ export class CandidatoDetailComponent implements OnInit {
       console.log('No tiene fecha de asignacion');
       this.mostrarAsignacionCandidato = true;
     }
+  }
+
+  protected onError(errorMessage: string) {
+    this.jhiAlertService.error(errorMessage, null, null);
+  }
+
+  protected paginateTareas(data: ITarea[], headers: HttpHeaders) {
+    // this.links = this.parseLinks.parse(headers.get('link'));
+    // this.totalItems = parseInt(headers.get('X-Total-Count'), 10);
+    this.tareas = data;
+
+    this.tareaCand = this.tareas.filter(tar => tar.candidatoId === this.candidato.id);
+  }
+
+  protected paginateBitacoras(data: IBitacora[], headers: HttpHeaders) {
+    // this.links = this.parseLinks.parse(headers.get('link'));
+    // this.totalItems = parseInt(headers.get('X-Total-Count'), 10);
+    this.bitacoras = data;
+
+    this.bitacorasCand = this.bitacoras.filter( bit => bit.candidatoId === this.candidato.id);
+  }
+
+  protected paginateSkillCandidatoes(data: ISkillCandidato[], headers: HttpHeaders) {
+    // this.links = this.parseLinks.parse(headers.get('link'));
+    // this.totalItems = parseInt(headers.get('X-Total-Count'), 10);
+    this.skills = data;
+
+    this.skillsCand = this.skills.filter( skill => skill.idCandidatoId === this.candidato.id);
   }
 }
 
